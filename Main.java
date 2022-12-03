a94167f8af1729ae6a0aeb6a1367212a3cebf4d2
jahyeon9908 <57889014+jahyeon9908@users.noreply.github.com>
2022-12-04 03:19:53 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ public class Main {
         }
         Collections.sort(computers); //Collections 클래스는 데이터와 관련된 작업들을 처리할 수 있게 도와주는 클래스
                                      //Collections 안의 메소드들은 전부 static이라 인스턴스로 만들 필요없이 바로 사용 가능
-                                     //sort는 인자로 list데이터타입 받고 집어넣으면 정렬해줌
+                                     //sort는 인자로 list데이터타입 받고 집어넣으면 정렬해준다
         System.out.println("\nafter");
         i = computers.iterator();
         while(i.hasNext()){
